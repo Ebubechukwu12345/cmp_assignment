6f4a5dd5dbd34639215263fc9e25d259b35c4df7
Ebubechuckwu12345 <dremzoyung@gmail.com>
2023-07-13 17:20:50 -0700

    sum of number

---
+++ numbers.py
@@ -0,0 +1,10 @@
+
+#math functions
+name = input('What is your name?: ')
+age = input('How old are you?: ')
+if age>'18':
+      print(name + age +
+            "You are eligible to vote")
+else:
+    print(name + age +
+          "Sorry not elegible to vote")
